1fd118bb6b8c3941f0eb7523d0489996b3c7844a
Dirk Peters <rocksandwoks@gmail.com>
2017-06-16 17:08:19 -0400

    Add routes for index, labels, artists, albums, songs. Add header & footer.

--- app/router.js
+++ app/router.js
@@ -7,6 +7,10 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('labels');
+  this.route('artists');
+  this.route('albums');
+  this.route('songs');
 });
 
 export default Router;
