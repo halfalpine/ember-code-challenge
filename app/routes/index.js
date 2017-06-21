dcc70137787a29e5f9273ab49adfa2b75505d6e0
Dirk Peters <rocksandwoks@gmail.com>
2017-06-20 22:28:22 -0400

    Add display artist to labels page

--- app/routes/index.js
+++ app/routes/index.js
@@ -39,15 +39,15 @@ export default Ember.Route.extend({
       let controller = this.get('controller');
       let label = this.store.createRecord('label', {
         name: controller.get('newLabel'),
-        artists: this.store.createRecord('artist', {
+        artists: [this.store.createRecord('artist', {
           name: controller.get('newArtist'),
-          album: this.store.createRecord('album', {
+          album: [this.store.createRecord('album', {
             name: controller.get('newAlbum'),
-            song: this.store.createRecord('song', {
+            song: [this.store.createRecord('song', {
               name: controller.get('newSong')
-            })
-          })
-        })
+            })]
+          })]
+        })]
       });
 
       return label.save().then(function() {
